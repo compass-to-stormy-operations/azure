ca573cbfea7213cba8824a16edfc00aee2d6c16c
Heitor Rapcinski <heitor.rapcinski@gmail.com>
2024-05-08 16:42:00 -0300

    Update script.ps1

--- 01-basics/script.ps1
+++ 01-basics/script.ps1
@@ -5,17 +5,31 @@ $randomIdentifier = (New-Guid).ToString().Substring(0,8)
 $subscription="Free Tier"
 $location="brazilsouth"
 $resourceGroup="alpha-resource-group"
+$vnetName="alpha-vnet"
+$subnetName = "alpha-snet"
+$vnetAddressPrefix = "10.0.0.0/16"
+$subnetAddressPrefix = "10.0.0.0/24"
 
-echo "randomIdentifier=$randomIdentifier"
-echo "Using resource group $resourceGroup"
-
-echo "Creating $resourceGroup in $location..."
 $ErrorActionPreference = "Stop"
 try {
+    #create a resource group
+    echo "Creating $resourceGroup in $location..."
     az group create --name $resourceGroup --location $location --subscription $subscription
     if ($? -eq $false) {
         throw 'Resource Group create failed.'
     }
+
+    # Create a virtual network and subnet
+    echo "Creating $vnetName -> $vnetAddressPrefix"
+    echo "Creating $subnetName -> subnetAddressPrefix"
+    az network vnet create --name $vnetName `
+      --resource-group $resourceGroup `
+      --address-prefixes $vnetAddressPrefix `
+      --subnet-name $subnetName `
+      --subnet-prefixes $subnetAddressPrefix
+    if ($? -eq $false) {
+        throw 'virtual network and subnet create failed.'
+    }      
 } catch {
     Write-Error $PSItem.Exception.Message
 }
