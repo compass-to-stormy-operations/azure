3b18548f7aa16bbb01a9c4b7961ed4621249aefc
Heitor Rapcinski <heitor.rapcinski@gmail.com>
2024-05-08 17:50:41 -0300

    Update script.ps1

--- 02 - bastions/script.ps1
+++ 02 - bastions/script.ps1
@@ -5,29 +5,27 @@ $randomIdentifier = (New-Guid).ToString().Substring(0,8)
 $location="brazilsouth"
 $resourceGroup="alpha-resource-group"
 $vnetName="alpha-vnet"
-$bastionHost="alpha-vnet-bastion"
+$bastionName="alpha-vnet-bastion"
 $publicIpName="alpha-vnet-ip"
 
 $ErrorActionPreference = "Stop"
 try {
     # Create a public ip
-    #echo "Creating Public IP $publicIpName"
-    #az network public-ip create `
-    #    --resource-group $resourceGroup `
-    #    --name $publicIpName
-    #if ($? -eq $false) {
-    #    throw 'public ip create failed.'
-    #}  
+    echo "Creating Public IP $publicIpName"
+    az network public-ip create `
+        --resource-group $resourceGroup `
+        --name $publicIpName
+    if ($? -eq $false) {
+        throw 'public ip create failed.'
+    }  
 
     # Create a bastion in virtual network
     echo "Creating Bastion Host $bastionHost in $vnetName"
     az network bastion create `
-        --name $bastionHost `
+        --name $bastionName `
         --public-ip-address $publicIpName `
         --resource-group $resourceGroup `
-        --vnet-name $vnetName `
-        --location $location `
-        --sku Basic
+        --vnet-name $vnetName
     if ($? -eq $false) {
         throw 'bastion create failed.'
     }      
