930611e2938fd9b8d3d7b4b67dc5b5cccf52e18d
Heitor Rapcinski <heitor.rapcinski@gmail.com>
2024-05-08 16:52:41 -0300

    Update script.ps1

--- 01 - resourcegroup + vnet + subnet/script.ps1
+++ 01 - resourcegroup + vnet + subnet/script.ps1
@@ -1,1 +1,36 @@
+# Create basic resource group
 
+# Variable block
+$randomIdentifier = (New-Guid).ToString().Substring(0,8)
+$subscription="Free Tier"
+$location="brazilsouth"
+$resourceGroup="alpha-resource-group"
+$vnetName="alpha-vnet"
+$subnetName = "alpha-snet"
+$vnetAddressPrefix = "10.0.0.0/16"
+$subnetAddressPrefix = "10.0.0.0/24"
+
+$ErrorActionPreference = "Stop"
+try {
+    #create a resource group
+    echo "Creating $resourceGroup in $location..."
+    az group create --name $resourceGroup --location $location --subscription $subscription
+    if ($? -eq $false) {
+        throw 'Resource Group create failed.'
+    }
+
+    # Create a virtual network and subnet
+    echo "Creating $vnetName -> $vnetAddressPrefix"
+    echo "Creating $subnetName -> $subnetAddressPrefix"
+    az network vnet create --name $vnetName `
+      --resource-group $resourceGroup `
+      --address-prefixes $vnetAddressPrefix `
+      --subnet-name $subnetName `
+      --subnet-prefixes $subnetAddressPrefix
+    if ($? -eq $false) {
+        throw 'virtual network and subnet create failed.'
+    }      
+} catch {
+    Write-Error $PSItem.Exception.Message
+}
+$ErrorActionPreference = "Continue"
