32d10dc4620a84f989fba8bc2bb1a7aaf010ff9b
Heitor Rapcinski <heitor.rapcinski@gmail.com>
2024-05-07 21:53:10 -0300

    Update script.ps1

--- 01-basics/script.ps1
+++ 01-basics/script.ps1
@@ -1,1 +1,10 @@
+# Create basic resource group
 
+# Variable block
+location="East US"
+resource-group="alpha-resource-group"
+
+echo "Using resource group $resource-group"
+
+echo "Creating $resource-group in $location..."
+az group create --name $resource-group --location "$location"
