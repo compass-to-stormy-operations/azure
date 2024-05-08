fb819af1518300472e62e23ccfc3970a5d675f39
Heitor Rapcinski <heitor.rapcinski@gmail.com>
2024-05-07 22:38:00 -0300

    Update script.ps1

--- 01-basics/script.ps1
+++ 01-basics/script.ps1
@@ -7,7 +7,14 @@ $resourceGroup="alpha-resource-group"
 echo "Using resource group $resourceGroup"
 
 echo "Creating $resourceGroup in $location..."
-az group create --name $resourceGroup --location $location
-if ($? -eq $false) {
-    Write-Error "Error creating resource group $resourceGroup"
+$ErrorActionPreference = "Stop"
+try {
+    az group create --name $resourceGroup --location $location
+    if ($? -eq $false) {
+        throw 'Group create failed.'
+    }
 }
+catch {
+    Write-Error "Error creating the resource group."
+}
+$ErrorActionPreference = "Continue"
