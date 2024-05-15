c1d1ffc3c6d9340c52b69c08bebdfc10546b940f
Heitor Rapcinski <heitor.rapcinski@gmail.com>
2024-05-15 11:44:29 -0300

    Create CreateStorageAccount.ps1

---
+++ CreateStorageAccount.ps1
@@ -0,0 +1,23 @@
+# Variable block
+$uniqueId="alpha"
+$name="storageaccount"
+$random=Get-Random
+$storageAccountName="$uniqueId$name$random"
+$resourceGroup="$uniqueId-resource-group"
+$sku="Standard_LRS"
+
+$ErrorActionPreference = "Stop"
+try {
+    # Create a storage account
+    Write-Output "Creating bastion snet"
+    az storage account create `
+      --name $storageAccountName `
+      --resource-group $resourceGroup `
+      --sku Standard_RAGRS 
+    if ($? -eq $false) {
+        throw 'bastion subnet create failed.'
+    }  
+} catch {
+    Write-Error $PSItem.Exception.Message
+}
+$ErrorActionPreference = "Continue"
